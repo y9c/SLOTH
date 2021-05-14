da269c4fa13a18296f3328e92781ba6410d39493
Ye Chang <yech1990@gmail.com>
2021-05-15 01:21:35 +0800

    Add doc of smrt pipeline

--- smrt/map_ccs_by_minimap2.sh
+++ smrt/map_ccs_by_minimap2.sh
@@ -6,12 +6,16 @@
 # Created: 2019-10-31 02:48
 # Discription: run_mapping
 
+# generate the index files first
+# minimap2 -d ref.mmi ref.fa
+
 input_bam=$1
 output_bam=$2
 threads=24
+ref="./hmf32.mmi"
 
 samtools fastq ${input_bam} |
-	minimap2 -t ${threads} -O 2,12 -E 2,1 --score-N 0 --end-bonus 100 -a --MD -x map-pb ./ref/hmf32.mni - |
-	samtools sort -@ ${threads} >${output_bam}
+  minimap2 -t ${threads} -O 2,12 -E 2,1 --score-N 0 --end-bonus 100 -a --MD -x map-pb ${ref} - |
+  samtools sort -@ ${threads} >${output_bam}
 
 samtools index -@ ${threads} ${output_bam}
